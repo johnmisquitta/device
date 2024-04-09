08aca28e3a86905222cbbc4b9a935c8efc9d2ecd
johnmisquitta <34851009+johnmisquitta@users.noreply.github.com>
2024-04-09 13:35:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from streamlit_gsheets import GSheetsConnection
 url = "https://docs.google.com/spreadsheets/d/1h3YyXq6n-_-2I_N7DuvdD-srgeMDPD5zINgbi5rGseU/edit#gid=0"
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
 
-url = "https://docs.google.com/spreadsheets/d/1h3YyXq6n-_-2I_N7DuvdD-srgeMDPD5zINgbi5rGseU/edit#gid=0"
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
+
+editable_df = st.dataframe(data, editable=True)
+
+# Update the Google Sheet with the edited DataFrame
+if st.button("Update Google Sheet"):
+    conn.write(editable_df, worksheet="YOUR_WORKSHEET_NAME_HERE")
+
+# Display the updated DataFrame
+st.dataframe(editable_df)
